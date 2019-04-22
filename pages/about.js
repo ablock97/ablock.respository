60422eb128d4c3ece51309978a2b2431021bc587
ablock97 <34139374+ablock97@users.noreply.github.com>
2019-04-21 21:49:55 -0500

    This is Gatsby Tutorial Part 3

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,8 @@
 import React from "react"
-import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-     <Header headerText="About Gatsby" /> 
-     <Header headerText="It's pretty cool" /> 
-    <p>Such wow. Very React.</p>
+  <div>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
   </div>
 )
-
